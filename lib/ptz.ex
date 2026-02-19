2985c5edf32c704a945961d929883119562a3716
moses <muirumoses9@gmail.com>
2026-02-19 12:51:33 +0300

    renaming and clean up

--- lib/ptz.ex
+++ lib/ptz.ex
@@ -10,7 +10,7 @@ defmodule ExOnvif.PTZ do
   import ExOnvif.Utils.Parser
   import SweetXml
 
-  alias ExOnvif.PTZ.{AbsoluteMove, Node, ServiceCapabilities, Status}
+  alias ExOnvif.PTZ.{AbsoluteMove, Node, ServiceCapabilities, Status, Preset}
 
   @doc """
   Operation to move pan,tilt or zoom to a absolute destination.
@@ -66,12 +66,12 @@ defmodule ExOnvif.PTZ do
   end
 
   @doc """
-  Operation to request all PTZ presets for the PTZNode in the selected profile. 
+  Operation to request all PTZ presets for the PTZNode in the selected profile.
   The operation is supported if there is support for at least on PTZ preset by the PTZNode.
   """
 
   @spec get_presets(ExOnvif.Device.t(), String.t()) ::
-          [ExOnvif.PTZ.Presets.preset_t()] | {:error, any()}
+          [Preset.preset_t()] | {:error, any()}
   def get_presets(device, profile_token) do
     body = element("tptz:GetPresets", element("tptz:ProfileToken", profile_token))
     ptz_request(device, "GetPresets", body, &parse_presets/1)
@@ -79,10 +79,10 @@ defmodule ExOnvif.PTZ do
 
   @doc """
 
-  The SetPreset command saves the current device position parameters so that the device can move to the saved preset position through the GotoPreset operation. 
-  In order to create a new preset, the SetPresetRequest contains no PresetToken. 
-  If creation is successful, the Response contains the PresetToken which uniquely identifies the Preset. 
-  An existing Preset can be overwritten by specifying the PresetToken of the corresponding Preset. In both cases (overwriting or creation) an optional PresetName can be specified. The operation fails if the PTZ device is moving during the SetPreset operation. 
+  The SetPreset command saves the current device position parameters so that the device can move to the saved preset position through the GotoPreset operation.
+  In order to create a new preset, the SetPresetRequest contains no PresetToken.
+  If creation is successful, the Response contains the PresetToken which uniquely identifies the Preset.
+  An existing Preset can be overwritten by specifying the PresetToken of the corresponding Preset. In both cases (overwriting or creation) an optional PresetName can be specified. The operation fails if the PTZ device is moving during the SetPreset operation.
   The device MAY internally save additional states such as imaging properties in the PTZ Preset which then should be recalled in the GotoPreset operation.
   """
   @spec set_preset(ExOnvif.Device.t(), String.t(), String.t(), String.t()) :: {:ok, String.t()}
@@ -116,7 +116,7 @@ defmodule ExOnvif.PTZ do
   """
   @spec goto_preset(ExOnvif.Device.t(), ExOnvif.PTZ.Presets.preset_t()) :: :ok
   def goto_preset(device, move) do
-    body = ExOnvif.PTZ.Presets.encode(move)
+    body = Preset.encode(move)
     ptz_request(device, "GotoPreset", body, fn _body -> :ok end)
   end
 
@@ -147,7 +147,7 @@ defmodule ExOnvif.PTZ do
         |> add_namespace("tt", "http://www.onvif.org/ver10/schema")
         |> add_namespace("tptz", "http://www.onvif.org/ver20/ptz/wsdl")
     )
-    |> ExOnvif.PTZ.Presets.to_struct()
+    |> Preset.to_struct()
   end
 
   defp parse_node_response(xml_response_body) do
